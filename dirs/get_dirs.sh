cd40d096a12e626268373cb7dbdb13275141538f
Renat Bekbolatov <rbekbolatov@avvo.com>
2015-09-02 09:30:30 -0700

    add more dir structure

---
+++ dirs/get_dirs.sh
@@ -0,0 +1,11 @@
+tree -L 2 ~/repos/gh  > dirs_repos
+tree -L 2 /usr/local/Cellar/  > dirs_brew
+
+tree -L 2 ~/.vim  > dirs_vim
+
+tree -L 1 /Library/Python/2.7/site-packages > dirs_python_site_packages
+tree -L 1 /System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python > dirs_python_extras
+
+
+
+
